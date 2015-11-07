17512fb77ae177647c176d92b1f294e6b076ffe6
Smart-Core <artem.digi@gmail.com>
2015-11-07 22:49:32 +0700

    clean ups

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -8,14 +8,11 @@ use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as SymfonyScriptHand
 class ScriptHandler extends SymfonyScriptHandler
 {
     /**
-     * Clears the APC/Wincache/Opcache cache.
-     *
      * @param $event CommandEvent A instance
      */
     public static function doctrineSchemaCheck(CommandEvent $event)
     {
         $options = parent::getOptions($event);
-        //$consoleDir = parent::getConsoleDir($event, 'clear the PHP Accelerator cache');
         $appDir = $options['symfony-app-dir'];
 
         if (null === $appDir) {
