3c660d8146dd9a267fc100a0ef4e24f8180ca20b
Smart-Core <artem.digi@gmail.com>
2016-10-10 02:36:46 +0700

    enable RegisterTwigExtensionsPass

--- SmartCoreBundle.php
+++ SmartCoreBundle.php
@@ -2,8 +2,14 @@
 
 namespace Smart\CoreBundle;
 
+use Smart\CoreBundle\DependencyInjection\Compiler;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SmartCoreBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new Compiler\RegisterTwigExtensionsPass($this));
+    }
 }
