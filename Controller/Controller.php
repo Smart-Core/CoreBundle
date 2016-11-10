2794b73f6acb633b24cc3d1d66341972b54eb639
Smart-Core <artem.digi@gmail.com>
2016-11-11 04:46:21 +0700

    fixes

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -4,25 +4,12 @@ namespace Smart\CoreBundle\Controller;
 
 use Smart\CoreBundle\Flash\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Doctrine\ORM\EntityRepository;
 
 /**
  * Inspired by KnpRadBundle
  */
 class Controller extends BaseController
 {
-    protected function getParameter($name)
-    {
-        return $this->container->getParameter($name);
-    }
-
-    protected function redirectToRoute($route, array $parameters = [], $status = 302)
-    {
-        return new RedirectResponse($this->get('router')->generate($route, $parameters), $status);
-    }
-
     // Doctrine
 
     protected function persist($object, $flush = false)
@@ -60,13 +47,6 @@ class Controller extends BaseController
         return $this->get('doctrine.orm.entity_manager')->getRepository($entityName);
     }
 
-    // Security
-
-    protected function isGranted($attributes, $object = null)
-    {
-        return $this->get('security.authorization_checker')->isGranted($attributes, $object);
-    }
-
     // Sessions and flashes
 
     protected function getSession()
@@ -76,6 +56,10 @@ class Controller extends BaseController
 
     protected function addFlash($type, $message = null, array $parameters = [], $pluralization = null)
     {
+        if (!$this->container->has('session')) {
+            throw new \LogicException('You can not use the addFlash method if sessions are disabled.');
+        }
+
         $message = $message ?: sprintf('%s.%s', $this->get('request_stack')->getMasterRequest()->attributes->get('_route'), $type);
 
         return $this->getSession()->getFlashBag()->add($type, new Message($message, $parameters, $pluralization));
